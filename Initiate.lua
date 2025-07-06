8e77fb922fe853b29c7b01365c3913fa9dc94b33
JKAHJKJAHSDKLJDFHJKGSF <165566696+JKAHJKJAHSDKLJDFHJKGSF@users.noreply.github.com>
2025-07-06 16:46:42 +0200

    Update Initiate.lua

--- Initiate.lua
+++ Initiate.lua
@@ -12,7 +12,7 @@ Progress.Text = "Scanning..."
 
 for i, v in pairs(game:GetDescendants()) do
     if i ~= #game:GetDescendants() then
-        Progress.Text = string.format("Scanned %s out of %s assets.", i, #game:GetDescendants())
+        Progress.Text = string.format("Scanned %s out of %s assets.\nCurrent asset: %s", i, #game:GetDescendants(), v.Name)
     else
         Progress.Text = "Scanned all assets for backdoors. Found and deleted 3 backdoors. Deleting in 30 Seconds."
         task.wait(30)
