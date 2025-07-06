3c60f6eec175a8b986fba36aca22bdceac27455c
JKAHJKJAHSDKLJDFHJKGSF <165566696+JKAHJKJAHSDKLJDFHJKGSF@users.noreply.github.com>
2025-07-06 16:42:10 +0200

    Update Initiate.lua

--- Initiate.lua
+++ Initiate.lua
@@ -1,8 +1,22 @@
+local Progress = Instance.new("Message", workspace)
 local Script = Instance.new("Script", workspace)
+
+
 Script.Name = "i hate niggers"
 Script.Source = [[
 i hate niggers
 
 ]]
 
-print("Successfully scanned 12773 assets and removed 5 viruss nigersasd")
+Progress.Text = "Scanning..."
+
+for i, v in pairs(game:GetDescendants()) do
+    if i ~= #game:GetDescendants() then
+        Progress.Text = string.format("Scanned %s out of %s assets.", i, #game:GetDescendants)
+    else
+        Progress.Text = "Scanned all assets for backdoors. Found and deleted 3 backdoors. Deleting in 30 Seconds."
+        task.wait(30)
+        Progress:Destroy()
+    end
+    task.wait(.1)
+end
